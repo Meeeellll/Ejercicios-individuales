016c2ae08e05975ae5a7683fe5a98ffaa7560de2
Melanie Elias <melamore15@gmail.com>
2022-11-14 05:15:24 -0600

    Ejercicios

---
+++ Ejercicio 4.js
@@ -0,0 +1,11 @@
+/* Escriba un programa que muestre la tabla de multiplicar del 1 al 10 del número ingresado por el usuario. */
+
+let tabla = 3;
+let i = 0;
+let resul;
+
+while(i<10) {
+    i++;
+    resul = tabla*i;
+    console.log(tabla + " X " +i+" = " + resul);
+}
